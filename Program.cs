6fd004ac88e5643d6acef2962eee3f27a1fc9f83
Lázaro Pimentel <142930247+LazaroBonfim@users.noreply.github.com>
2025-08-19 23:50:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "admin" && loginDTO.Senha == "password")
+    {
+        return Results.Ok("Login com sucesso!");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
+public class LoginDTO
+{
+    public string Email { get; set; } = default;
+    public string Senha { get; set; } =default;
+}
+
